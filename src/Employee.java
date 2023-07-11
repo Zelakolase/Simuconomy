52fe093c72ef24bd6b5aa246b5ca90dee8e7f8ef
Morad <460583232@sharkia1.moe.edu.eg>
2023-07-11 20:46:53 +0300

    Remaster

--- src/Employee.java
+++
@@ -1,9 +0,0 @@
-public class Employee {
-    public double Wealth;
-    public double Energy;
-    public double FearFactor;
-    public double AConsumptionFactor;
-    public double Salary;
-    public double ActualSkillandWellbeing = 1.0;
-    public double PotentialSkillandWellbeing = 1.0;
-}

57d36a043efa36100100339e305afce1dff27235
Morad <460583232@sharkia1.moe.edu.eg>
2023-07-12 09:41:12 +0300

    Agent implementation

--- src/Objects/Agent.java
+++ src/Objects/Agent.java
@@ -9,22 +9,22 @@ import Environment.GlobalVariables;
 public class Agent {
     public double previousPrice = GlobalVariables.startingPrice;
     /* The inflation index to previousPrice. If inflator = 1.5, the previousPrice will be inflated by 50% */
-    public double inflator = 1.1;
+    public double inflator = 1.0;
     /* 
      * The base inflator sensitivity is how much does the inflator increases for the first panic iteration.
      * Inflator will be increased by inflatorSensitivity and decreased by 1.0 - inflatorSensitivity.
      * Should not be higher than 1.0 !!!
      */
-    public double baseInflatorSensitivity = GlobalVariables.startingBaseInflatorSensitivity; // This is a genetic trait, modifiable
+    public double baseInflatorSensitivity; // This is a genetic trait, modifiable
     /*
      * Base supply capacity is the gene trait for the starting supply capacity
      */
-    public int baseSupplyCapacity = GlobalVariables.startingBaseSupplyCapacity; // This is a genetic trait, not-modifiable
+    public int baseSupplyCapacity; // This is a genetic trait, not-modifiable
     /*
      * Base demand capacity is the gene trait for the starting demand capacity as a percentage of baseSupplyCapacity
      * 0.5 means that demand will be 5 if supply is 10
      */
-    public double baseDemandCapacity = GlobalVariables.startingBaseDemandCapacity; // This is a genetic trait, not-modifiable
+    public double baseDemandCapacity; // This is a genetic trait, not-modifiable
     /*
      * How many units to produce every iteration
      */
@@ -43,9 +43,13 @@ public class Agent {
     /*
      * Wealth, initalized by a global variable
      */
-    public double wealth = GlobalVariables.startingWealth;
+    public double wealth;
     /*
      * Declare dead?
      */
     public boolean isDead = false;
+    /*
+     * How many children does the agent have
+     */
+    public int children = 0;
 }

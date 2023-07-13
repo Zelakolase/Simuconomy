e685f65a902178f9f8a82f8ad2e4f664f7b52414
Morad <460583232@sharkia1.moe.edu.eg>
2023-07-13 23:08:27 +0300

    Unimportant change

--- src/Environment/GlobalVariables.java
+++ src/Environment/GlobalVariables.java
@@ -6,15 +6,15 @@ package Environment;
  */
 public class GlobalVariables {
     /* How many agents to initialize */
-    public static int startingPopulation = 1_000;
+    public static int startingPopulation = 500;
     /* The initialized price */
     public static double startingPrice = 5.0;
     /* The initalized baseDemandCapacity */
-    public static double startingBaseDemandCapacity = 0.75;
+    public static double startingBaseDemandCapacity = 1.0;
     /* The initalized baseSupplyCapacity */
     public static int startingBaseSupplyCapacity = 10;
     /* The initalized baseInflatorSensitivity */
-    public static double startingBaseInflatorSensitivity = 0.25;
+    public static double startingBaseInflatorSensitivity = 0.1;
     /* How many iterations should the economy run */
     public static int iterations = 500;
 }
